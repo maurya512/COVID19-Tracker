b5d9375d5dea5ef9d22dc1311e6c931912077047
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-17 19:01:20 -0500

    the Cards now display real time data and can be seen as a counter counting upwards

--- covid19-tracker/src/components/Cards/Cards.jsx
+++ covid19-tracker/src/components/Cards/Cards.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+// a dependency to count up to give us a real world timer
+import CountUp from 'react-countup';
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
+// destructure "data" and then destructure further
+const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
+    console.log(confirmed);
+    if(!confirmed) {
+        return "Loading.....";
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
@@ -13,9 +19,16 @@ const Cards = (props) => {
                             Infected
                         </Typography>
                         {/* Real Data to be fetched from the api */}
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                        <CountUp 
+                        start={0}
+                        end={confirmed.value}
+                        duration={2.5}
+                        separator=','
+                        />
+                        </Typography>
                         {/* Real Date to be fetched from the api as well */}
-                        <Typography color="textSecondary">Real Date</Typography>
+    <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of Active Cases</Typography>
                     </CardContent>
                 </Grid>
@@ -25,9 +38,16 @@ const Cards = (props) => {
                             Recovered
                         </Typography>
                         {/* Real Data to be fetched from the api */}
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                            start={0}
+                            end={recovered.value}
+                            duration={2.5}
+                            separator=','
+                            />
+                        </Typography>
                         {/* Real Date to be fetched from the api as well */}
-                        <Typography color="textSecondary">Real Date</Typography>
+    <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of Recovered Cases</Typography>
                     </CardContent>
                 </Grid>
@@ -37,9 +57,16 @@ const Cards = (props) => {
                             Deaths
                         </Typography>
                         {/* Real Data to be fetched from the api */}
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                            start={0}
+                            end={deaths.value}
+                            duration={2.5}
+                            separator=','
+                            />
+                        </Typography>
                         {/* Real Date to be fetched from the api as well */}
-                        <Typography color="textSecondary">Real Date</Typography>
+    <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of Deaths</Typography>
                     </CardContent>
                 </Grid>

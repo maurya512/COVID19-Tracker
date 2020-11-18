b29f2d1680c38e41b69cff9e04062569e27701ee
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-18 12:41:51 -0500

    added a method to display all the countries in the world

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     // declaring the state like this also creates a constructor in the backend
     state = {
         data: {},
+        country: '',
     }
     // aysnc await on componentDitMount to check whether the app is receiving any data from the fetchData function 
     async componentDidMount() {
@@ -16,12 +17,19 @@ class App extends React.Component {
         const fetchedData = await fetchData();
         this.setState({ data: fetchedData})
     }
+
+    // async function
+    handleCountryChange = async (country) => {
+        console.log(country);
+        // fetch the data
+        // set the state
+    }
     render() {
         const { data } = this.state;
         return (
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker />
+                <CountryPicker handleCountryChange={this.handleCountryChange }/>
                 <Chart />
             </div>
         )

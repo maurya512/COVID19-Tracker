857789145a30a8c1d532661c4c724a5eeaa9c578
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-17 19:10:44 -0500

    made cards more appealing with real time data and accessible to all screen sizes

--- covid19-tracker/src/components/Cards/Cards.jsx
+++ covid19-tracker/src/components/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 // a dependency to count up to give us a real world timer
 import CountUp from 'react-countup';
 import styles from './Cards.module.css';
+import cx from "classnames";
 
 // destructure "data" and then destructure further
 const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
@@ -13,7 +14,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}> 
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Infected
@@ -32,7 +33,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of Active Cases</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Recovered
@@ -51,7 +52,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of Recovered Cases</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Deaths

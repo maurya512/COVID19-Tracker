d27ca95eb3227a7249d20fe70920f3db9fb799d8
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-18 14:15:42 -0500

    added a way to display info about each country's data in the card

--- covid19-tracker/src/api/index.js
+++ covid19-tracker/src/api/index.js
@@ -6,10 +6,16 @@ const url = "https://covid19.mathdro.id/api";
 // async await function
 // try and catch block
 // try is gonna be executed if the fetch is successful
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changableUrl = url;
+
+    // if a valid country is selected from the list then the data for that country will be populated
+    if(country) {
+        changableUrl = `${url}/countries/${country}`
+    }
     try {
         // data 
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changableUrl);
 
         const modifiedData = { 
             confirmed,

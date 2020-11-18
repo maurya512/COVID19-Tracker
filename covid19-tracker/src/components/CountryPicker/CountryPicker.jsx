9bfbefd0b6a743a1a11e423ad392a70cc9913a33
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-18 12:43:27 -0500

    async function to get data for each country and display the selected country in the console log

--- covid19-tracker/src/components/CountryPicker/CountryPicker.jsx
+++ covid19-tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
-const CountryPicker = () => {
-    return(<h1>
-        Country Picker
-    </h1>)
+import styles from './CountryPicker.module.css';
+
+import { fetchCountries } from '../../api';
+
+const CountryPicker = ({ handleCountryChange }) => {
+    const [fetchedCountries, setFetchCountries ] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+
+    }, [setFetchCountries]);
+
+    return(
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue=" " onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="global">Global</option>
+    {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
+    )
 }
 
 export default CountryPicker;

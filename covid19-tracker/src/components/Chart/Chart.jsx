506adbe1c4a3030b00c359cd223f1a1968fd044a
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-18 14:28:59 -0500

    fixed an issue where the api was being called endlessly in the console log

--- covid19-tracker/src/components/Chart/Chart.jsx
+++ covid19-tracker/src/components/Chart/Chart.jsx
@@ -4,15 +4,15 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: {confirmed, recovered, deaths}, country }) => {
     const [ dailyData, setDailyData] = useState([]);
     useEffect(() => {
         const fetchAPI = async ()=> {
         setDailyData(await fetchDailyData());
         }
-        console.log(dailyData);
+        // console.log(dailyData);
         fetchAPI();
-    }, [dailyData]);
+    }, []);
 
     // this chart is responsible for the global data
     const lineChart = (
@@ -36,9 +36,31 @@ const Chart = () => {
             }}
         />) : null
     );
+
+    const barChart = (
+        confirmed 
+        ? (
+            <Bar 
+            data={{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{
+                    label: 'People',
+                    backgroundColor: ['rgba(0,0,255,0.5)','rgba(0,255,0,0.5)','rgba(255,0,0,0.5)'],
+                    data: [confirmed, recovered, deaths]
+                }]
+            }}
+            options={{
+                legend: {display: false},
+                title: {display: true, text:`Current state in ${country}`},
+            }}
+            />
+        ) : null
+    ); 
+
+    console.log(confirmed, recovered, deaths);
     return(
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
